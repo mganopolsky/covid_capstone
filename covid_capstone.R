4ab057ec667db935cce0ca3e420bbbceb9afa5f8
mganopolsky <ganopolsky.marina@gmail.com>
2020-09-16 13:49:44 -0400

    Added my project

---
+++ covid_capstone.R
@@ -0,0 +1,14 @@
+## Importing packages
+
+library(tidyverse) # metapackage with lots of helpful functions
+library(lubridate)
+library(splines)
+
+## Reading in file
+
+dir <- "data"
+#files <- list.files(dir)
+#files
+file_name <- paste(dir, "covid_19_clean_complete.csv", sep = "/")
+all_data <- read_csv(file_name)
+
